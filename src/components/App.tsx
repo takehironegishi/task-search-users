9bf46b9075b5f5d3a5d823c6ca7e7913a86e0b77
takehironegishi <takehiro.n61@gmail.com>
2020-12-16 23:05:44 +0900

    implement reducer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import SearchForm from './SearchForm';
 import UsersList from "./UsersList";
+import UserContext from "../contexts/UserContext";
+import users from '../reducers';
 
 const App = (): JSX.Element => {
+  // TODO initialstateの型指定
+  const initialState: any = '';
+  const [state, dispatch] = useReducer(users, initialState);
+
   return (
-    <>
+    <UserContext.Provider value={{ state, dispatch }}>
       <SearchForm />
       <UsersList />
-    </>
+    </UserContext.Provider>
   );
-}
+};
 
 export default App;
---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// TODO 型指定
+const UserContext = createContext<any>('');
+
+export default UserContext;
